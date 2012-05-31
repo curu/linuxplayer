beec8d8e1a715b9a0c195ba2ee31cd93d075b404
prinbra <prinbra@gmail.com>
2012-05-31 15:05:38 +0000

    Changes: 1. add --output-dir and --all-tables option 2. refactor match algorithm

--- scripts/xmysqldump.pl
+++ scripts/xmysqldump.pl
@@ -9,12 +9,13 @@
 use strict;
 use warnings;
 
+use File::Path;
+use File::Spec;
 use Getopt::Long;
-use List::Util qw(sum);
 use POSIX;
 
-my $version = "1.0";
-my($opt_h,$opt_list, $opt_separate, $opt_v);
+my $version = "1.1";
+my($opt_h,$opt_list, $opt_separate, $opt_v, $opt_all, $opt_dir);
 my(@opt_databases, @opt_tables);
 our $debug = 0;
 
@@ -25,6 +26,9 @@ sub usage {
 	print "\n";
 	print "  $0 -l|--list [file.sql ...]\n";
 	print "    List databases and tables\n";
+	print "\n";
+	print "  $0 -a|--all-tables [file.sql ...]\n";
+	print "    split to one .sql file per table\n";
 	print "\n";
 	print "  $0 [-s|--separate] -d|--databases db1,dbn... [file.sql ...]\n";
 	print "    Extract selected databases:\n";
@@ -37,6 +41,7 @@ sub usage {
 	print "\n";
 	print "Options:\n";
 	print "    --debug print some debug information to STDERR\n";
+	print "    -o|--output-dir directory to output sql files\n";
 	print "    -s|--separate output separate sql files named like DB.TBL.sql\n";
 	print "    -v|--version show program version\n";
 	print "NOTE: \n";
@@ -49,24 +54,41 @@ sub log_msg {
     print STDERR "[$now]: @_\n" if $debug;
 }
 
+sub get_true_item{
+	my $hash = shift;
+	for my $key (keys %{$hash}){
+		return $key if $hash->{$key};
+	}
+	return undef;
+}
+
+
+sub create_file{
+	my $filename = shift;
+	if($opt_dir){
+		mkpath($opt_dir) unless -d $opt_dir;
+		$filename = File::Spec->catfile($opt_dir, $filename);
+	}
+	log_msg "create file '$filename'";
+	open(my $handle, ">", $filename) or 
+		die "unable to open '$filename' to write";
+	return $handle;
+}
 
 GetOptions(
 	'h|help' 	=> \$opt_h,
 	'debug'		=> \$debug,
+	'a|all-tables'	=> \$opt_all,
 	'd|databases=s'	=> \@opt_databases,
 	't|tables=s'	=> \@opt_tables,
 	'l|list'	=> \$opt_list,
+	'o|output-dir=s'=> \$opt_dir,
 	's|separate'	=> \$opt_separate,
 	'v|version'	=> \$opt_v,
 	);
 
-my $CHARSET_RE = qr{\Q/*!40101 SET NAMES\E};
-my $DB_RE = qr/^-- Current Database: .\w+./;
 my $DB_RE_CAP = qr/^-- Current Database: .(\w+)./;
-my $TBL_RE = qr/^-- Table structure for table .\w+./;
 my $TBL_RE_CAP = qr/^-- Table structure for table .(\w+)./;
-my $DB_RE_END = qr/^-- Current Database:/;
-my $TBL_RE_END = qr/^-- Table structure for/;
 
 #check option
 if($opt_v){
@@ -90,7 +112,7 @@ if($opt_list){
 	exit(0)
 }
 
-if($opt_h || !(@opt_databases || @opt_tables)){
+if($opt_h || !(@opt_databases || @opt_tables || $opt_all)){
 	usage();
 	exit(0);
 }
@@ -102,144 +124,182 @@ if(@opt_databases && @opt_tables){
 #extract selected databases
 if(@opt_databases){
 	my @databases =  split(/,/,join(',',@opt_databases));
-	my @db_re;
 	my $db_cnt = scalar @databases;
 	my $end_cnt = 0;
-	my $got_charset = "";
+	my $in_header = 1;
+	my $dump_header = "";
 
+	my %in_db;
+	my ($cur_db,$prev_db);
+	my $out_handle;
+	
 	for my $db (@databases){
-		my $re = qr/^-- Current Database: .\Q$db\E./;
-		my $handle;
-		if($opt_separate){
-			my $out_file = "$db.sql";
-			open($handle, ">", $out_file) or die "unable to open '$out_file' for write";
-		}
-		push @db_re, { db => $db, re => $re, start => 0, 'end' => 0, 'handle' => $handle };
+		$in_db{$db} = 0;
 	}	
 
 	line: while(<>){
-		#get charset comment
-		$got_charset = $_ if(!$got_charset && /$CHARSET_RE/);
+		$prev_db = get_true_item(\%in_db);
 
-		for my $d (@db_re){
-			my $re = $d->{'re'};
-			if(!$d->{'start'} && /$re/){
-				$d->{'start'} = 1;
-				log_msg "find db " . $d->{'db'};
-				log_msg $_;
-				select $d->{'handle'} if $d->{'handle'};
-				print $got_charset;
-				next;
-			}
-			if($d->{'start'} && !$d->{'end'} && (/$DB_RE_END/ || eof())){
-				$d->{'end'} = 1;
-				log_msg "end db " . $d->{'db'};
-				log_msg $_;
-				$end_cnt++;
-				next;
-			}
-			#output mathed lines
-			if($d->{'start'} && !$d->{'end'}){
-				print;
-				next line;
-			}
+		if(eof() && $prev_db){
+			log_msg "End DB [eof] '$prev_db'";
+			last;
 		}
+
+		if(/$DB_RE_CAP/){
+			$in_header = 0;
+			$cur_db = $1;
+
+			my $found = 0;
+			if($prev_db){
+				$found = 1;
+				$end_cnt++;
+				log_msg "End DB '$prev_db'";
+				$in_db{$prev_db} = 0;
+			}
+
+			if (exists($in_db{$cur_db})){
+				$found = 1;
+				log_msg "Found DB '$cur_db'";
+				$in_db{$cur_db} = 1;
+				if($opt_separate){
+					my $out_file = "$cur_db.sql";
+					my $handle = create_file($out_file);
+					select $handle if $handle;
+					close($out_handle) if $out_handle;
+					$out_handle = $handle;
+				}
+				print $dump_header;
+			}
+			log_msg "$_" if $found;
+			
+		}
+		$dump_header .= $_ if($in_header);
+	
+		$prev_db = get_true_item(\%in_db);
+		print if $prev_db;
+
 		#if all requested databases extracted, we are done
 		if($end_cnt == $db_cnt){
 			last line;
 		}
 	}
-	for my $d (@db_re){
-		close($d->{'handle'}) if $d->{'handle'};
-	}
+	close($out_handle) if $out_handle;
 	exit(0);
 }
+
 #extract selected tables
-if(@opt_tables){
+if(@opt_tables || $opt_all){
 	my @tables =  split(/,/,join(',',@opt_tables));
-	my @table_re;
 	my $table_cnt = scalar @tables;
 	my $end_cnt = 0;
-	my %db_start;
-	my %db_end;
-	my $got_charset = "";
+
+	my $in_header = 1;
+	my $dump_header = "";
+	my (%in_db, %in_table);
+	my ($cur_db, $cur_table);
+	my ($prev_db, $prev_table);
+	my $out_handle;
 
 	for my $table (@tables){
 		my ($db_name, $tbl_name) = split /\./, $table;
 		#if it's not in db.name format
-		if(!$tbl_name){
-			$tbl_name = $db_name;
-			$db_name = "";
-		}
-		my $re = qr/^-- Table structure for table .\Q$tbl_name\E./;
-		my $re2 = $db_name ? qr/^-- Current Database: .\Q$db_name\E./ : undef;
-		my $handle;
-		#add to db hash
-		$db_start{$db_name} = 0 if $db_name;
-		if($opt_separate){
-			my $out_file = $db_name ? "$db_name.$tbl_name.sql" : "$tbl_name.sql";
-			open($handle, ">", $out_file) or die "unable to open '$out_file' for write";
-		}
-		push @table_re, { db => $db_name, dbre => $re2,
-			db_start => 0, db_end => 0, 
-			table => $tbl_name, re => $re, 
-			start => 0, 'end' => 0 ,
-			'handle' => $handle};
+		if($tbl_name){
+			$in_db{$db_name} = 0;
+		}	
+		$in_table{$table} = 0;
 	}	
 
-	line: while(<>){
-		#get charset comment
-		$got_charset = $_ if(!$got_charset && /$CHARSET_RE/);
-		for my $d (@table_re){
-			my $re = $d->{'re'};
-			my $dbre = $d->{'dbre'};
-			my $db = $d->{'db'};
-			#log_msg $d->{'table'};
-			#position target db
-			if($db){
-				if(!$db_start{$db} && /$dbre/){
-					$db_start{$db} = 1;
-					log_msg "find db " . $db;
-					log_msg $_;
-					next line;
-				}
-				if($db_start{$db} && !$db_end{$db} && (/$DB_RE_END/ || eof())){
-					log_msg "end db " . $db;
-					log_msg $_;
-					$db_end{$db} = 1;
-					next;
-				}
-				#skip if we are not in the right database
-				next if !$db_start{$db} || $db_end{$db};
+	while(<>){
+		$prev_table = get_true_item(\%in_table);
+		$prev_db = get_true_item(\%in_db);
+		
+		#end of file, just for debug purpose
+		if(eof()){
+			if($prev_table){
+				log_msg "End Table [eof] '$prev_table'";
+				print "-- End table '$prev_table'\n\n";
 			}
-			if(!$d->{'start'} && /$re/){
-				log_msg "find table " . $d->{'table'};
-				log_msg $_;
-				$d->{'start'} = 1;
-				select $d->{'handle'} if $d->{'handle'};
-				print $got_charset;
-				printf 'USE DATABASE `%s`;'."\n", $d->{'db'} if $d->{'db'};
-				next;
+
+			log_msg "End DB [eof] '$prev_db'" if $prev_db;
+			last;
+		}
+
+		#locate database
+		if(/$DB_RE_CAP/){
+			$in_header = 0;
+			$cur_db = $1;
+
+			my $found = 0;
+			if($prev_db){
+				$found = 1;
+				if($prev_table){
+					log_msg "End Table '$prev_table'";
+					print "-- End table '$prev_table'\n\n";
+					$in_table{$prev_table} = 0;
+					$prev_table = undef;
+					$end_cnt++;
+				}
+				log_msg "End DB '$prev_db'";
+				$in_db{$prev_db} = 0;
 			}
-			if($d->{'start'} && !$d->{'end'} && (/$TBL_RE_END/ || /$DB_RE_END/ || eof())){
-				$d->{'end'} = 1;
-				log_msg "end table " . $d->{'table'};
-				log_msg $_;
+
+			if (exists($in_db{$cur_db})){
+				$found = 1;
+				log_msg "Found DB '$cur_db'";
+				$in_db{$cur_db} = 1;
+			}
+			log_msg "$_" if $found;
+			next;
+			
+		}
+		$dump_header .= $_ if($in_header);
+
+		#locate table
+		if($cur_db && /$TBL_RE_CAP/){
+			$in_header = 0;
+			$cur_table = $1;
+			$cur_table = $cur_db ? "$cur_db.$cur_table" : "$cur_table";
+			my $found = 0;
+			if($prev_table){
+				$found++;
 				$end_cnt++;
-				next;
+				$in_table{$prev_table} = 0;
+				log_msg "End Table '$prev_table'";
+
+				#output table end
+				print "-- End table '$prev_table'\n\n";
 			}
-			if($d->{'start'} && !$d->{'end'}){
-				print;
-				next line;
+			if(exists($in_table{$cur_table}) || $opt_all){
+				$found++;
+				$in_table{$cur_table} = 1;
+				log_msg "Found table '$cur_table'";
+
+				 if ($opt_separate || $opt_all){
+					##output table start
+					my $out_file = "$cur_table.sql";
+					my $handle = create_file($out_file);
+					select $handle if $handle;
+					close($out_handle) if $out_handle;
+					$out_handle = $handle;
+				}
+				print $dump_header;
+				print $_;
+
+
 			}
+			log_msg "$_" if $found;
+		}else{
+			my $in_table = get_true_item(\%in_table);
+			print if($in_table)
+
 		}
+
 		#if all requested databases extracted, we are done
-		if($end_cnt == $table_cnt){
-			last line;
+		if(!$opt_all && ($end_cnt == $table_cnt)){
+			log_msg "End DB [last tbl]'$prev_db'" if $prev_db;
+			log_msg "Done";
+			last;
 		}
 	}
-	for my $d (@table_re){
-		close($d->{'handle'}) if $d->{'handle'};
-	}
-	exit(0);
+	close($out_handle) if $out_handle;
 }
